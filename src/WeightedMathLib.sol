7bfec4715f6dafad4efc2e7c4f2b7c5b6d823cc3
hungngo <qhung.bm0409@gmail.com>
2024-01-22 23:33:08 +0700

    feat: change solidty version from 0.8.21 to 0.8.19

--- src/WeightedMathLib.sol
+++ src/WeightedMathLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.21;
+pragma solidity >=0.8.19;
 
 import "solady/src/utils/FixedPointMathLib.sol";
 import "solady/src/utils/SafeCastLib.sol";
